5a7df10a8686b1c1a36973178b828b4f69040cae
Aryanarya01 <aryanarya2777@gmial.com>
2025-09-11 18:18:32 +0530

    single number

---
+++ 1st.cpp
@@ -0,0 +1,37 @@
+//                                                                  leatcode problem 136
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+
+// int main(){
+
+//     vector<int> nums={1,3,1,2,2};
+//     int ans=0;
+//     for(int val :nums){
+//         ans^= val;
+//     }
+//     cout<<ans<<endl;
+//     return 0;
+// }
+
+//                                                        vector linear search
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+  
+
+// int main(){
+    
+//     vector<int> nums={1,3,2,21,4};
+//      int target = 3;
+//      int index =0;
+//      for(int val : nums){
+//         if(val == target){
+//             cout<<val<<"at index"<<index<<endl;
+//             break;
+//         }
+//         index++;
+//      }
+//     return 0;
+// }
