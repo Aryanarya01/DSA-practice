c11443f4980d1f8912bff04fff0e172c9ba13655
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-09-27 16:32:08 +0530

    Selection sort

---
+++ selectionSort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int selectionSort(int arr[],int n ){//O(n^2)
+        for(int i=0;i<n-1;i++){
+            int smallIdx = i;//unsorted part starting
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[smallIdx]){
+                smallIdx = j;
+                }
+            }
+            swap(arr[i],arr[smallIdx]);
+        }
+}
+
+int printArr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+int n =5;
+int arr[] = {4,1,5,2,3};
+printArr(arr,n);
+ selectionSort(arr, n);
+
+ cout << "After sorting: ";
+ printArr(arr, n);
+
+ 
+
+    return 0;
+}
