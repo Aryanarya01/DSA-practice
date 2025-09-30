dbaf967791d090b09fec6be32c09ebdccbd018a0
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-09-30 17:22:49 +0530

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,3 +1,42 @@
+// #include<iostream>
+// using namespace std;
+
+// int selectionSort(int arr[],int n ){//O(n^2)
+//         for(int i=0;i<n-1;i++){
+//             int smallIdx = i;//unsorted part starting
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[j]<arr[smallIdx]){
+//                 smallIdx = j;
+//                 }
+//             }
+//             swap(arr[i],arr[smallIdx]);
+//         }
+// }
+
+// int printArr(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// int main(){
+
+// int n =5;
+// int arr[] = {4,1,5,2,3};
+// printArr(arr,n);
+//  selectionSort(arr, n);
+
+//  cout << "After sorting: ";
+//  printArr(arr, n);
+
+ 
+
+//     return 0;
+// }
+
+
+//                  in descending order
 #include<iostream>
 using namespace std;
 
@@ -5,7 +44,7 @@ int selectionSort(int arr[],int n ){//O(n^2)
         for(int i=0;i<n-1;i++){
             int smallIdx = i;//unsorted part starting
             for(int j=i+1;j<n;j++){
-                if(arr[j]<arr[smallIdx]){
+                if(arr[j]>arr[smallIdx]){
                 smallIdx = j;
                 }
             }
