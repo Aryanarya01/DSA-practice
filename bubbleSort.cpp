b870ac19f6f885761bc9b01c80e7e39c4fa5d8f4
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-10-01 07:25:51 +0530

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,16 +1,54 @@
+//                   print bubble sort in ascending order
+
+// #include<iostream>
+// using namespace std;
+
+// void bubbleSort(int arr[],int n){//O(n^2)
+//     for(int i=0;i<n-1;i++){
+//         bool isSwap = false;
+//         for(int j=0;j<n-i-1;j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//                 isSwap = true; 
+//             }
+//         }
+//         if(!isSwap){//array is already sorted
+//             return;
+//         }
+//     }
+// }
+
+// void printArr(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// int main(){
+ 
+// int n = 5;
+// int arr[]={4,1,5,2,3};
+// bubbleSort(arr,n);
+// printArr(arr,n);
+//     return 0;
+// }
+
+
+//                                 in descending order
+
 #include<iostream>
 using namespace std;
-
-void bubbleSort(int arr[],int n){//O(n^2)
+void bubbleSort(int arr[],int n){
     for(int i=0;i<n-1;i++){
         bool isSwap = false;
         for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
-                isSwap = true; 
+            if(arr[j+1]>arr[j]){
+                swap(arr[j+1],arr[j]);
+                isSwap = true;
             }
         }
-        if(!isSwap){//array is already sorted
+        if(!isSwap){
             return;
         }
     }
@@ -24,10 +62,11 @@ void printArr(int arr[],int n){
 }
 
 int main(){
- 
-int n = 5;
-int arr[]={4,1,5,2,3};
-bubbleSort(arr,n);
-printArr(arr,n);
-    return 0;
+
+ int n = 5;
+ int arr[]={4,1,5,2,3};
+ bubbleSort(arr,n);
+ printArr(arr,n);
+
+return 0;
 }
