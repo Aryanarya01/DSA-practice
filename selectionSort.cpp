f886aab875be799c61620cf1fc781ff40183e833
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-10-04 15:14:38 +0530

    Updated selection sort implementation

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -36,23 +36,24 @@
 // }
 
 
+ 
 //                  in descending order
 #include<iostream>
 using namespace std;
-
-int selectionSort(int arr[],int n ){//O(n^2)
-        for(int i=0;i<n-1;i++){
-            int smallIdx = i;//unsorted part starting
-            for(int j=i+1;j<n;j++){
-                if(arr[j]>arr[smallIdx]){
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int smallIdx = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]>arr[smallIdx]){
+ 
                 smallIdx = j;
-                }
             }
             swap(arr[i],arr[smallIdx]);
         }
+    }
 }
 
-int printArr(int arr[],int n){
+void printArr(int arr[],int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
@@ -63,13 +64,9 @@ int main(){
 
 int n =5;
 int arr[] = {4,1,5,2,3};
+selectionSort(arr,n);
 printArr(arr,n);
- selectionSort(arr, n);
 
- cout << "After sorting: ";
- printArr(arr, n);
-
- 
 
     return 0;
 }
