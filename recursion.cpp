f7e21ccd4f8681e68926aaf568b05e4b91661db4
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-11-05 16:00:52 +0530

    recursion concepts

---
+++ recursion.cpp
@@ -0,0 +1,57 @@
+// #include<iostream>
+// using namespace std;
+
+// void printNumber(int n){
+//     if(n==1){
+//         cout<<"1\n";
+//         return;
+//     }
+//     cout<<n<<" ";
+//     printNumber(n-1);
+// }
+
+// int main(){
+//     int n = 5;
+//     printNumber(n);
+
+//     return 0;
+// }
+
+
+//     factorial of n
+
+// #include<iostream>
+// using namespace std;
+
+// int factorial(int n){
+//     if(n==0){
+//         return 1;
+//     }
+//     return n*factorial(n-1);
+// }
+// int main(){
+//     int n = 4;
+//     cout<<factorial(n);
+//     return 0;
+// }
+
+
+// sum of n number
+
+// #include<iostream>
+// using namespace std;
+
+// int sumN(int n){
+//     if(n==1){
+//         return 1;
+//     }
+//     return n + sumN(n-1);
+// }
+
+
+// int main(){
+
+//     int n = 5;
+//     cout<<sumN(n);
+//     return 0;
+// }
