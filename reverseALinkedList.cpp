90d7207a51151a8d040505c7c58822b454efb62c
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-11-07 16:09:12 +0530

    reversing a linkedList

---
+++ reverseALinkedList.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+ struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+class Solution{
+    public:
+    ListNode* reverseList(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+};
+
+int main(){
+     
+  ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    // 2️⃣ Reverse function call karo
+    Solution obj;
+    ListNode* newHead = obj.reverseList(head);
+
+    // 3️⃣ Reversed list print karo
+    cout << "Reversed List: ";
+    while (newHead != NULL) {
+        cout << newHead->val << " ";
+        newHead = newHead->next;
+    }
+    cout<<endl;
+
+    return 0;
+}
