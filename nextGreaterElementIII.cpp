e3359a49a86d71b3900c8779fe4925f63aed2d37
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-11-28 17:33:03 +0530

    initial commit

---
+++ nextGreaterElementIII.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int>& nums){
+    int n = nums.size();
+    vector<int> ans(n,-1);
+    stack<int> s;
+
+    for(int i=2*n-1;i>=0;i--){
+        while(s.size()>0 && nums[s.top()] <= nums[i%n]){
+            s.pop();
+        }
+
+        ans[i%n] = s.empty() ? -1 : nums[s.top()];
+        s.push(i%n);
+    }
+    return ans;
+}
+int main(){
+    vector<int> nums = {3,6,5,4,2};
+
+    vector<int> result = nextGreaterElement(nums);
+    for(int val : result){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+}
