356134d2550077ee7a89bd5f3356ea769a0b7a0f
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-12-06 16:23:09 +0530

    Sliding Window Maximum

---
+++ SlidingWindowMax.cpp
@@ -0,0 +1,43 @@
+// Sliding Window Maximum problem
+
+#include<iostream>
+#include<queue>
+#include<deque>
+#include<vector>
+using namespace std;
+ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    deque<int> dq;
+    vector<int> ans;
+
+    for(int i=0;i<k;i++){
+        while(dq.size()>0 && nums[dq.back()]<= nums[i]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+
+    for(int i=k;i<nums.size();i++){
+        ans.push_back(nums[dq.front()]);
+        while(dq.size()>0 && dq.front() <= i-k){
+            dq.pop_front();
+        }
+         while(dq.size()>0 && nums[dq.back()]<= nums[i]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+    ans.push_back(nums[dq.front()]);
+    return ans;
+ }
+int main(){
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+    vector<int> res = maxSlidingWindow(nums, k);
+
+    for(int x : res) {
+        cout << x << " ";
+    }
+
+
+    return 0;
+}
