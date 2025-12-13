1cae1cd5cfaabd2f61da701daee9ba750db3e591
Aryanarya01 <aryanaryaa2006@gmail.com>
2025-12-13 22:03:50 +0530

    initial commit

---
+++ sumOfNodes.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+    static int idx = -1;
+Node* buildTree(vector<int>& preorder){
+        idx++;
+         if(preorder[idx] == -1){ 
+        return NULL;
+    }
+        Node* root = new Node(preorder[idx]);
+        root->left = buildTree(preorder);
+        root->right = buildTree(preorder);
+        return root;
+}
+
+int Sum(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftSum = Sum(root->left);
+    int rightSum = Sum(root->right);
+    return leftSum + rightSum + root->data;
+}
+int main(){
+
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildTree(preorder);
+        cout<<"sum :"<<Sum(root)<<endl;
+
+    return 0;
+}
